5ae2bdff14beabed84bde446ccc7fdb56f187d7c
Maro Huang <ayuhamaro@gmail.com>
2013-09-08 23:26:21 +0800

    修正縮排

--- mysql.base.php
+++ mysql.base.php
@@ -1,30 +1,30 @@
 <?php
-	class mysqlBase
-	{
-		private static $Db;
-		//建構子建立新資料庫連線
-		public function __construct()
-		{
-			self::$Db = new mysqli('host','dbUserId','dbUserPassword','dbName');
-			//設定資料庫讀取用UTF-8
-			self::$Db->query('SET NAMES utf8');
-			self::$Db->query('SET CHARACTER_SET_CLIENT=utf8');
-			self::$Db->query('SET CHARACTER_SET_RESULTS=utf8');
-		}
-		//解構子
-		public function __destruct(){}
-		//一般查詢
+    class mysqlBase
+    {
+        private static $Db;
+        //建構子建立新資料庫連線
+        public function __construct()
+        {
+            self::$Db = new mysqli('host','dbUserId','dbUserPassword','dbName');
+            //設定資料庫讀取用UTF-8
+            self::$Db->query('SET NAMES utf8');
+            self::$Db->query('SET CHARACTER_SET_CLIENT=utf8');
+            self::$Db->query('SET CHARACTER_SET_RESULTS=utf8');
+        }
+        //解構子
+        public function __destruct(){}
+        //一般查詢
         public static function query($queryStr)
         {
             return self::$Db->query($queryStr);
         }
-		//插入並傳回ID
+        //插入並傳回ID
         public static function insertAndGetId($queryStr)
         {
             self::$Db->query($queryStr);
             return self::$Db->insert_id;
         }
-		//預備敘述式，可以避免資料注入
+        //預備敘述式，可以避免資料注入
         public static function prepare($queryStr)
         {
             return self::$Db->prepare($queryStr);
@@ -54,5 +54,5 @@
             $result->free();
             return $row[0];
         }
-	}
+    }
 ?>
